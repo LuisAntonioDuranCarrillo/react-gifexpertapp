dba3fb3311309564cebcf78dc181aa9abadb2886
Antonio <luis.ant238@gmail.com>
2021-07-29 10:37:40 -0500

    Mi primer commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,21 @@
+
+
+
+export const getGifs = async (category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=rgNtmQMG8DgWp8bVca7ftKsIF2wMbCaG`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifs;
+
+
+}
